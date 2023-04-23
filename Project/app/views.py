14c7411b03039ca710d334b5b95bb4bbb21f059a
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-04-23 21:15:50 +0300

    Change views

--- Project/app/views.py
+++ Project/app/views.py
@@ -1,35 +1,55 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from .models import User
-from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, validate_not_empty
 
-@api_view(['POST'])
-def auth(request):
-    # Implement authentication logic here
-    return Response(status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-class UserListAPIView(generics.ListAPIView):
-    serializer_class = UserSerializer
+class UserCreateAPIView(generics.CreateAPIView):
+    """
+    API view to create a new user.
+    """
     queryset = User.objects.all()
-    pagination_class = LimitOffsetPagination
+    serializer_class = UserSerializer
 
-@api_view(['GET'])
-def list_users(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-@api_view(['PUT'])
-def edit_user(request, user_id):
-    try:
-        user = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = UserSerializer(user, data=request.data)
-    if serializer.is_valid():
+    def perform_create(self, serializer):
+        """
+        Override perform_create method to hash the password before saving it to the database.
+        """
+        password = serializer.validated_data.get('password', None)
+        if password is not None:
+            serializer.validated_data['password'] = make_password(password)
         serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Override post method to validate the data before creating the user.
+        """
+        data = request.data.copy()
+        validate_not_empty(data.get('username', ''))
+        validate_not_empty(data.get('email', ''))
+        validate_not_empty(data.get('password', ''))
+        return super().post(request, *args, **kwargs)
+
+
+class UserListAPIView(generics.ListAPIView):
+    """
+    API view to list all users.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """
+    API view to update a user.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_update(self, serializer):
+        """
+        Override perform_update method to hash the password before saving it to the database.
+        """
+        password = serializer.validated_data.get('password', None)
+        if password is not None:
+            serializer.validated_data['password'] = make_password(password)
+        serializer.save()
