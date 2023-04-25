265ab46b0b3dfdce5940bdc4e3721401be7fa8ad
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-04-25 14:03:40 +0300

    test

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
