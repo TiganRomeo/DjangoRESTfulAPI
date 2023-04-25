5195a72bdea93026a150f962bc86baea5c869902
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-04-25 14:29:20 +0300

    a

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,7 +13,7 @@ from rest_framework import generics
 from .serializers import UserSerializer
 
 @api_view(['POST'])
-@authentication_classes([TokenAuthentication])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def auth_view(request):
     username = request.data.get('username')
