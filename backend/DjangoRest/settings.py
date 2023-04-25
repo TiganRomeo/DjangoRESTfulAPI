7527db277c8883332bda1793ce08e9284fe6d126
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-04-25 14:14:30 +0300

    test

--- backend/DjangoRest/settings.py
+++ backend/DjangoRest/settings.py
@@ -85,10 +85,9 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
