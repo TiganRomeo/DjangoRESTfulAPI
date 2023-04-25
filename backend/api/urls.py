9555a83cef43cc5070ac95c4ebb24010ea652b6c
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-04-25 16:25:42 +0300

    test

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,10 +4,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import auth_view, UserAddView, UserListView, UserEditView
 
 urlpatterns = [
-    path('auth/', auth_view),
-    path('user/add/', UserAddView.as_view()),
-    path('user/list/', UserListView.as_view()),
-    path('user/edit/<int:pk>/', UserEditView.as_view()),
+    path('auth/', auth_view, name="Auth"),
+    path('user/add/', UserAddView.as_view(), name="Add-User"),
+    path('user/list/', UserListView.as_view(), name="User-List"),
+    path('user/edit/<int:pk>/', UserEditView.as_view(), name="Edit-User"),
     path('api-token-auth/', obtain_auth_token),
 ]
 
